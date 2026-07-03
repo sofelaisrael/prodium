74e91d2778f5b9570d9c3e7870dba55ed34b0996
sofelaisrael <129384534+sofelaisrael@users.noreply.github.com>
2026-07-03 09:25:51 +0100

    files

--- server/app.js
+++ server/app.js
@@ -30,7 +30,7 @@ app.use(
       }
     },
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
+    allowedHeaders: ["Content-Type", "Authorization", "x-file-ext"],
     credentials: true,
   }),
 );
